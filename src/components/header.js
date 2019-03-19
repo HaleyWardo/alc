336bfd00c4e57674ca48bf7f50da7857a5ce680b
Haley <hayevans18@gmail.com>
2019-03-19 15:30:54 -0400

    Add styles for header and add nav links

--- src/components/header.js
+++ src/components/header.js
@@ -5,28 +5,104 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `white`,
+      boxShadow: `0 2px 4px 0 rgba(0,0,0,0.2)`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
+        alignItems: `center`,
+        display: `flex`,
+        justifyContent: `space-between`,
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: 1024,
+        // maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 
+        style={{ 
+          margin: 0,
+          display: `flex`
+        }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#333333`,
+            fontSize: `25px`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
+
+      <nav style={{width: `60%`}}>
+        <ul 
+          style={{
+            display: `flex`,
+            fontSize: `16px`,
+            justifyContent: `space-around`,
+            margin: `0`
+          }}>
+          <Link
+            style={{
+              color: `#659bdb`,
+              textDecoration: `none`,
+            }}
+            to="/home"
+          >Home</Link>
+
+          <Link
+            style={{
+              color: `#659bdb`,
+              textDecoration: `none`,
+            }}
+            to="/about"
+          >About</Link>
+
+          <Link
+            style={{
+              color: `#659bdb`,
+              textDecoration: `none`,
+            }}
+            to="/services"
+          >Services</Link>
+
+          <Link
+            style={{
+              color: `#659bdb`,
+              textDecoration: `none`,
+            }}
+            to="/projects"
+          >Projects</Link>
+
+          <Link
+            style={{
+              color: `#659bdb`,
+              textDecoration: `none`,
+            }}
+            to="/news"
+          >News</Link>
+
+          <Link
+            style={{
+              color: `#659bdb`,
+              textDecoration: `none`,
+            }}
+            to="/contact"
+          >Contact Us</Link>
+
+          <Link
+            style={{
+              color: `#659bdb`,
+              textDecoration: `none`,
+            }}
+            to="/careers"
+          >Careers</Link>
+        </ul>
+      </nav>
     </div>
   </header>
 )
