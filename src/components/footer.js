d6b287356ee02de59852a4306ee0dd5f064122ac
Haley <hayevans18@gmail.com>
2019-03-19 17:50:44 -0400

    Add footer and styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,98 @@
+import { Link } from "gatsby"
 import React from 'react';
 
-const Footer = () => {
+class Footer extends React.Component {
+    render() {
+        return (
+            <div 
+                style={{
+                    alignItems: `center`,
+                    backgroundColor: `#163f6f`,
+                    display: `flex`,
+                    flexDirection: `column`,
+                    padding: `35px`
+            }}>
+                <nav 
+                    style={{
+                        marginBottom: `40px`,
+                        width: `60%`,
+                    }}>
+                    <ul 
+                    style={{
+                        display: `flex`,
+                        fontSize: `16px`,
+                        justifyContent: `space-around`,
+                        margin: `0`,
+                        textTransform: `uppercase`
+                    }}>
+                    <Link
+                        style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                        }}
+                        to="/home"
+                    >Home</Link>
 
+                    <Link
+                        style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                        }}
+                        to="/about"
+                    >About</Link>
+
+                    <Link
+                        style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                        }}
+                        to="/services"
+                    >Services</Link>
+
+                    <Link
+                        style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                        }}
+                        to="/projects"
+                    >Projects</Link>
+
+                    <Link
+                        style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                        }}
+                        to="/news"
+                    >News</Link>
+
+                    <Link
+                        style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                        }}
+                        to="/contact"
+                    >Contact Us</Link>
+
+                    <Link
+                        style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                        }}
+                        to="/careers"
+                    >Careers</Link>
+                    </ul>
+                </nav>
+
+                <span 
+                    style={{
+                        fontSize: `15px`
+                    }}
+                >
+                    &copy;Copyright. All rights reserved.
+                </span>
+            </div>
+        );
+    }
 }
 
 export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+import Footer from "./footer"
 import Header from "./header"
 import "./layout.css"
 
@@ -26,21 +27,20 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
+
         <div
           style={{
             margin: `0 auto`,
             maxWidth: 960,
+            minHeight: `1024px`,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
         >
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
         </div>
+
+        <Footer />
       </>
     )}
   />
